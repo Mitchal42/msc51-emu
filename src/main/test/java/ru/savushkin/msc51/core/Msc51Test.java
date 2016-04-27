7d508d39db69c73de822cf0201e545370b2622c5
Ivan Savushkin <ivansavuskin@gmail.com>
2016-04-28 00:06:43 +0300

    tests

--- src/main/test/java/ru/savushkin/msc51/core/Msc51Test.java
+++ src/main/test/java/ru/savushkin/msc51/core/Msc51Test.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
@@ -37,7 +38,7 @@ public class Msc51Test {
 
     @Test
     public void orlARjTest() {
-        msc51.setCode(Arrays.asList(0x46L));
+        msc51.setCode(Collections.singletonList(0x46L));
         msc51.setAcc(msc51.getAcc().setBits(0x52));
         msc51.setData(0x0, new DataType(8, 0x6D));
         msc51.setData(0x6D, new DataType(8, 0x49));
@@ -49,5 +50,20 @@ public class Msc51Test {
         System.out.println(msc51.internalRegsToString());
         assertEquals("PC is't correct", 1, msc51.getPc().toNumber());
         assertEquals("ACC is't correct", "0x5b", "0x" + Long.toHexString(msc51.getAcc().toNumber()));
+    }
+
+    @Test
+    public void orlAImTest() {
+        msc51.setCode(Arrays.asList(0x44L, 0x42L));
+        msc51.setAcc(msc51.getAcc().setBits(0x84));
+        msc51.setData(0x6D, new DataType(8, 0x49));
+        System.out.println(msc51.disassemble((int) msc51.getPc().toNumber()));
+        do {
+            msc51.execute();
+        } while (msc51.getCycle() != 0);
+        System.out.println(msc51.regsToString());
+        System.out.println(msc51.internalRegsToString());
+        assertEquals("PC is't correct", 2, msc51.getPc().toNumber());
+        assertEquals("ACC is't correct", "0xc6", "0x" + Long.toHexString(msc51.getAcc().toNumber()));
     }
 }
